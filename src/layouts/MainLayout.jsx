079af4037ff0d84605becb46c818168ea4f52422
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2025-06-03 23:26:41 +0600

    The foundational project setup is now complete.

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <header>
+                <Navbar/>
+            </header>
+            <main className='min-h-screen'>
+                <Outlet/>
+            </main>
+            <footer>
+                <Footer/>
+            </footer>
+        </div>
+    );
+};
+
+export default MainLayout;
